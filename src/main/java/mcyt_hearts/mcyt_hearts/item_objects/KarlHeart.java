af0a0de03e367ea29d1d5b05068c42b97462a040
gamma_02 <gammagames06@gmail.com>
2021-10-18 19:43:01 -0700

    d

---
+++ src/main/java/mcyt_hearts/mcyt_hearts/item_objects/KarlHeart.java
@@ -0,0 +1,25 @@
+package mcyt_hearts.mcyt_hearts.item_objects;
+
+import mcyt_hearts.mcyt_hearts.components.player.hearts.HeartComponent;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class KarlHeart extends Heart
+{
+    public KarlHeart(Settings settings)
+    {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand)
+    {
+
+        HeartComponent.HEART_COMPONENT.get(user).addHeart(this);
+        user.getInventory().removeOne(this.getDefaultStack());
+        return super.use(world, user, hand);
+    }
+}
--- src/main/java/mcyt_hearts/mcyt_hearts/items.java
+++ src/main/java/mcyt_hearts/mcyt_hearts/items.java
@@ -1,6 +1,6 @@
 package mcyt_hearts.mcyt_hearts;
 
-import mcyt_hearts.mcyt_hearts.item_objects.TechnoHeart;
+import mcyt_hearts.mcyt_hearts.item_objects.*;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
@@ -27,16 +27,16 @@ public class items {
             });
 
 
-    public static final Item DREAM_HEART = new Item(new FabricItemSettings().group(creativeModGroup));
+    public static final Item DREAM_HEART = new DreamHeart(new FabricItemSettings().group(creativeModGroup));
     public static final Item TECHNO_HEART = new TechnoHeart(new FabricItemSettings().group(creativeModGroup));
-    public static final Item TOMMY_HEART = new Item(new FabricItemSettings().group(creativeModGroup));
-    public static final Item GEORGE_HEART = new Item(new FabricItemSettings().group(creativeModGroup));
-    public static final Item APHMAU_HEART = new Item(new FabricItemSettings().group(creativeModGroup));
-    public static final Item PRESTON_HEART = new Item(new FabricItemSettings().group(creativeModGroup));
-    public static final Item MRBEAST_HEART = new Item(new FabricItemSettings().group(creativeModGroup));
-    public static final Item CRAFTEE_HEART = new Item(new FabricItemSettings().group(creativeModGroup));
-    public static final Item WISP_HEART = new Item(new FabricItemSettings().group(creativeModGroup));
-    public static final Item KARL_HEART = new Item(new FabricItemSettings().group(creativeModGroup));
+    public static final Item TOMMY_HEART = new TommyHeart(new FabricItemSettings().group(creativeModGroup));
+    public static final Item GEORGE_HEART = new GeorgeHeart(new FabricItemSettings().group(creativeModGroup));
+    public static final Item APHMAU_HEART = new AphmauHeart(new FabricItemSettings().group(creativeModGroup));
+    public static final Item PRESTON_HEART = new PrestonHeart(new FabricItemSettings().group(creativeModGroup));
+    public static final Item MRBEAST_HEART = new MrbeastHeart(new FabricItemSettings().group(creativeModGroup));
+    public static final Item CRAFTEE_HEART = new CrafteeHeart(new FabricItemSettings().group(creativeModGroup));
+    public static final Item WISP_HEART = new WispHeart(new FabricItemSettings().group(creativeModGroup));
+    public static final Item KARL_HEART = new KarlHeart(new FabricItemSettings().group(creativeModGroup));
 
 
     static {
@@ -60,7 +60,6 @@ public class items {
     public static void registerItems() {
         for(ItemRegistryHelper item : items) {
             Registry.register(Registry.ITEM, item.identifier, item.item);
-            //Devilrycraft.LOGGER.log(Level.INFO, "registering item : " + item.identifier);
         }
     }
 
